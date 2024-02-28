8a3f9e9cc80fc12a4203acde32466eeb371e8035
hans-grimlund <hans.grimlund@icloud.com>
2024-02-28 15:54:23 +0100

    Moved project

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+export const Navbar = () => {
+    return (
+        <div className="my-navbar col-12 bg-dark d-flex align-items-center">
+            <div className="col-4">
+
+            </div>
+            <div className="col-4 d-flex justify-content-center">
+                <Link to="/" className='lead'>The Weather Site</Link>
+            </div>
+            <div className="col-4">
+
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+export default () => {
+
+    return (
+        <div className="col-12 d-flex flex-column align-items-center pt-4">
+            <div className="d-flex justify-content-center col-12 row">
+            </div>
+        </div>
+    )
+}
